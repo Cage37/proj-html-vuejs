9895cb691aaf6567932fac565ab88ebcf26dc401
Cage37 <casiraghi.smn@gmail.com>
2021-06-15 18:35:27 +0200

    First section main complete

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,6 +4,6 @@ let mix = require('laravel-mix');
 
 mix
 .js('src/app.js', 'js')
-.sass('src/app.scss', 'css')
+.sass('src/app.scss', 'css').options({ processCssUrls: false })
 .copyDirectory('src/images', 'dist/images')
 .setPublicPath('dist');
