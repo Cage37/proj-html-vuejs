907b49ee4c4f6aab4344e9164c9286b5b3ec3bf3
Cage37 <casiraghi.smn@gmail.com>
2021-06-15 11:58:30 +0200

    Structure complete

---
+++ webpack.mix.js
@@ -0,0 +1,9 @@
+// webpack.mix.js
+
+let mix = require('laravel-mix');
+
+mix
+.js('src/app.js', 'js')
+.sass('src/app.scss', 'css')
+.copyDirectory('src/images', 'dist/images')
+.setPublicPath('dist');
