70f3d0e484c56c5004bda2b6215f3ecf9856133a
Cage37 <casiraghi.smn@gmail.com>
2021-06-17 15:33:05 +0200

    Footer complete

--- dist/js/app.js
+++ dist/js/app.js
@@ -28,6 +28,39 @@ var app = new Vue({
     }, {
       nameLink: "Shop",
       link: ""
+    }],
+    linkFooterExplore: [{
+      nameLink: "Start here",
+      link: ""
+    }, {
+      nameLink: "Blog",
+      link: ""
+    }, {
+      nameLink: "About us",
+      link: ""
+    }],
+    linkFooterContact: [{
+      nameLink: "Success story",
+      link: ""
+    }, {
+      nameLink: "Courses",
+      link: ""
+    }, {
+      nameLink: "Contact us",
+      link: ""
+    }],
+    linkFooterInfo: [{
+      nameLink: "Membership",
+      link: ""
+    }, {
+      nameLink: "Purchase guide",
+      link: ""
+    }, {
+      nameLink: "Privacy policy",
+      link: ""
+    }, {
+      nameLink: "Terms of service",
+      link: ""
     }]
   },
   methods: {},
